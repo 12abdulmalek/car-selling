1fbfa789ba6cb0112f6ae00c6deddb8e7cbfb605
12abdulmalek <abdul35-585@diu.edu.bd>
2021-11-26 20:20:57 +0600

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+#  live link
+ it is live link :  https://sad-meitner-f092a8.netlify.app
+
+## server link : 
+https://github.com/programming-hero-web-course-4/niche-website-server-side-12abdulmalek
+
+## client link : 
+https://github.com/programming-hero-web-course-4/niche-website-client-side-12abdulmalek
+### how to work this website
+
+this website is mainly focus on car seller and car design .. 
---
+++ src/Pages/Home/Review/Review.js
@@ -0,0 +1,34 @@
+import { Rating } from '@mui/material';
+// import Rating from "react-rating-stars-component";
+import Button from '@restart/ui/esm/Button';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
+
+const Review = () => {
+  const [review,setReview] = useState([])
+  useEffect(()=>{
+     const url = `https://desolate-mesa-41652.herokuapp.com/review`;
+     fetch(url)
+     .then(res => res.json())
+     .then(data => {
+       setReview(data);
+     })
+  },[])
+    return (
+        <div style={{margin:"12"}}>
+          <h3 style={{margin:"12"}}>Review System</h3>
+           {
+             review.map(item=><div>
+             <div>
+               <p> {item.coment} </p>
+               <Rating name="half-rating" 
+               defaultValue={item.rating}
+                precision={0.5} />
+             </div>
+             </div>)
+           }
+        </div>
+    );
+};
+
+export default Review;
